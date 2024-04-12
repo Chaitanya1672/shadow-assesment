d6ec8c1f90f0bddabe5c2f033a45db1928c23826
Chaitanya Narhare <chetannarhare111@gmail.com>
2024-04-12 10:04:09 +0530

    added suspense boundary for searchParams use

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import DataGridComponent from "../components/DataGridComponent";
+import React, { Suspense } from 'react';
 
 export const metadata = {
   title: "Data Grid Table",
 };
 
 export default function Page() {
-  return <DataGridComponent />;
+  return(
+    //Suspense for useSearchParams
+    <Suspense fallback="Loading...">
+      <DataGridComponent />;
+    </Suspense>
+  )
 }
